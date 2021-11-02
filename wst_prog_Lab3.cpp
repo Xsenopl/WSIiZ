bc31d59fe1e2db985f5755669a70a72057ea5166
Xsenopl <49074078+Xsenopl@users.noreply.github.com>
2021-11-02 18:16:38 +0100

    podstawy

---
+++ wst_prog_Lab3.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+//____________________________________________________
+	int a, i{ 0 };
+	cout << "Wprowadz liczbe: ", cin >> a;
+	for (int i = 0; i < a; i++) cout << i << ", ";
+cout << endl;
+	while (i < a)				cout << i++ << ", ";
+cout << endl; i = 0;
+	do {
+		cout << i++ << ", ";
+	} while (i < a);
+//____________________________________________________
+
+cout << endl;
+	do {
+		cin >> a;
+	} while (a >= 0);
+//____________________________________________________
+
+	cout << endl << "Wpisuj liczby: ";
+	int sumaP{ 0 }, sumaM{ 0 }, dod{ 0 }, uj{ 0 };
+	for (int i = 0; i < 10; i++)
+	{
+		cin>> a;
+		if (a > 0)		{ sumaP += a; dod++; }
+		else if (a < 0) { sumaM += a; uj++; }
+		else continue;
+	}
+	cout << "Ilosc dodatnich= " << dod << "\t\tIlosc ujemnych= " << uj << endl;
+	cout << "Suma dodatnich= " << sumaP << "\t\tSuma ujemnych= " << sumaM << endl;
+//___________________________________________________
+
+	int n{ 10 }; sumaP = 0; sumaM = 0;
+	cout << endl << "Zakres (z kodu) liczb do "<<n<<endl;
+	for (int i=0; i<=n; i++) {
+		if (i % 2 == 0) sumaP += i;
+		else continue;
+	}
+	cout << "Suma liczb parzystych= " << sumaP <<endl;
+//___________________________________________________
+
+	sumaP=0;
+	cout << endl << "Podaj n= ", cin >> n;
+	srand(time(NULL));
+	for (int b,i = 0; i <= n; i++) {
+		b = rand() % 34 + 11;
+		cout << b << endl;
+		if (b % 2 == 0) sumaP += b;
+		else continue;
+	}
+	cout << "Suma liczb parzystych= " << sumaP << endl;
+
+
+
+return 0;
+}
---
+++ wst_prog_Lab3_cw2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i{1}, n, sumaP{0};
+	cout << "Podaj liczbe studentow: ", cin >> n;
+	while(n<1||n>100) cout << "Podaj WLASCIWA liczbe studentow: ", cin >> n;
+
+	while (i <= n) {
+		int punkt;
+		cin >> punkt;	
+			if (punkt >= 0 && punkt<=100)
+				sumaP += punkt;
+			else continue;
+		i++;
+
+	}
+	cout << "Srednia punktow w grupie = " << (float)sumaP / n<<endl;
+//________NIESKOŃCZONA PĘTLA______________________________________________________________________________________
+/*	sumaP = 0, i = 1;
+	while (true) {
+		int punkt;
+		cin >> punkt;
+
+		if (punkt >= 0 && punkt <= 100)
+			sumaP += punkt;
+		else continue;
+		i++;
+
+	}
+	cout << "Srednia punktow w grupie = " << (float)sumaP / n << endl;
+
+
+//__________PĘTLA DO_WHILE_______________________________________________________________
+	sumaP = 0;
+	 do{
+		int punkt;
+		cin >> punkt;
+
+		if (punkt >= 0 && punkt <= 100)
+			sumaP += punkt;
+		else continue;
+		i++;
+
+	 } while (true);
+	cout << "Srednia punktow w grupie = " << (float)sumaP / n << endl;
+*/
+//____________________________________________________________
+	cout << "ZADANIE 5\n";
+	for (int j = 1; j <= 100; j++)	cout << j << " ";
+	cout << "\n\n";
+	for (int j = 100; j >= 0; j--)	cout << j << " ";
+	cout << "\n\n";
+	for (int j = 7; j <= 77; j+=7)	cout << j << " ";
+	cout << "\n\n";
+	for (int j = 20; j >= 0; j-=2)	cout << j << " ";
+	cout << "\n\n";
+
+}
